4247aa80c06958833b1664dab3a3f19f27a3f2a7
yasinsunmaz <yasinsunmaz13@gmail.com>
2022-11-26 22:34:13 +0300

    SRP - Genel açıklamalar eklendi.

--- SRP.SingleResponsibilityPrinciple/Program.cs
+++ SRP.SingleResponsibilityPrinciple/Program.cs
@@ -7,13 +7,21 @@ Database db = new Database();
 db.Connect();
 db.GetPersons();
 
-Console.WriteLine("* Tüm Kişi Listesi *");
+Console.WriteLine("*** Tüm Kişi Listesi ***");
 foreach (var item in db.GetPersons())
 {
   Console.WriteLine(item.FirstName);
 }
 
-Console.WriteLine("* Kişi Bul *");
+Console.WriteLine("** Kişi Bul **");
+
+//SRP uygun olmayan bir durum. Kişi listesi üzerinden biri çekmek. Sebebi bu durumu sürekli yaptığınız kod maliyeti artar.
+var personNotIdeal = db.GetPersons().Where(x => x.Id == 1).FirstOrDefault();
+string firstNameNotIdeal = personNotIdeal.FirstName;
+string lastNameNotIdeal = personNotIdeal.LastName;
+
+//SRP metot düzeyinde doğru bir kullanım. Çünkü kişiyi getiren bir metot var. Oldukça basit ve anlaşılır.
+//Fakat sorun db üzerinden bu metota erişiyor olmamızdır.
 var person = db.GetPersonInfo(1);
 if (person.Id > 0)
 {
@@ -34,20 +42,27 @@ dbService.Connect();
 
 PersonService personService = new PersonService();
 
-Console.WriteLine("* Tüm Kişi Listesi **");
+Console.WriteLine("*** Tüm Kişi Listesi ***");
 foreach (var item in personService.GetPersons())
 {
   Console.WriteLine(item.FirstName);
 }
 
 Console.WriteLine("** Kişi Bul **");
-var presonInfo = personService.GetPersonInfo(1);
-if (presonInfo.Id > 0)
+var personInfo = personService.GetPersonInfo(1);
+if (personInfo.Id > 0)
 {
-  Console.WriteLine("* Bulunan Kişi: " + presonInfo.FirstName + " " + presonInfo.LastName);
+  Console.WriteLine("* Bulunan Kişi: " + personInfo.FirstName + " " + personInfo.LastName);
 }
 else
 {
   Console.WriteLine("Kişi bulunamadı");
 }
 dbService.Disconnect();
+
+///<summary>
+///SRP örneklerde görüldüğü üzere amaç satır, metot ve sınıf düzeyinde tek bir sorumluluğu yerine getirmesi esastır.
+///Satır 52'deki kullanım satır düzeyinde doğru bir kullanımdır.
+///Satır 19'daki kullanıma göre satır 25'deki kullanım metot düzeyinde doğru bir kullanımdır.
+///Satır 25'deki kullanıma göre satır 52'deki kullanım sınıf düzeyinde doğru bir kullanımdır.
+/// </summary>
